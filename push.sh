228a6c4ccee2c62de49e4a488b122cccb6dc92ee
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-08-13 21:45:35 +0000

    dashboard user-list user-profile ...

--- push.sh
+++ push.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+# Ajouter tous les fichiers modifiés
 git add .
+
+# Commit avec message multi-lignes
 git commit -m "
 dashboard
 user-list
 user-profile
+...
 "
+
+# Pousser sur Gitea
+git push gitea main
+
+# Pousser sur GitHub
 git push origin main
-git gitea main
