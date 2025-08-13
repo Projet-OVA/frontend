5e4b4064a525f0ff125868f5e710e99778940a79
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-08-13 20:27:30 +0000

    toutes les pages minimales fonctionnent

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "structure minimale"
+git commit -m "toutes les pages minimales fonctionnent"
 git push
--- sira/src/app/app.routes.ts
+++ sira/src/app/app.routes.ts
@@ -1,75 +1,75 @@
 import { Routes } from '@angular/router';
 
 // Import des composants (à adapter si noms différents)
-import { DashboardComponent } from './dashboard/dashboard';
-import { LoginComponent } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
 
 // Badges
-import { BadgesListComponent } from './badges/badges-list/badges-list';
-import { BadgesCreateComponent } from './badges/badges-create/badges-create';
-import { BadgesEditComponent } from './badges/badges-edit/badges-edit';
+import { BadgesList } from './badges/badges-list/badges-list';
+import { BadgesCreate } from './badges/badges-create/badges-create';
+import { BadgesEdit } from './badges/badges-edit/badges-edit';
 
 // Challenges
-import { ChallengesListComponent } from './contents/challenges/challenges-list/challenges-list';
-import { ChallengesCreateComponent } from './contents/challenges/challenges-create/challenges-create';
-import { ChallengesEditComponent } from './contents/challenges/challenges-edit/challenges-edit';
-import { ParticipationsComponent } from './contents/challenges/participations/participations';
+import { ChallengesList } from './contents/challenges/challenges-list/challenges-list';
+import { ChallengesCreate } from './contents/challenges/challenges-create/challenges-create';
+import { ChallengesEdit } from './contents/challenges/challenges-edit/challenges-edit';
+import { Participations } from './contents/challenges/participations/participations';
 
 // Educational
-import { EducationalListComponent } from './contents/educational/educational-list/educational-list';
-import { EducationalCreateComponent } from './contents/educational/educational-create/educational-create';
-import { EducationalEditComponent } from './contents/educational/educational-edit/educational-edit';
+import { EducationalList } from './contents/educational/educational-list/educational-list';
+import { EducationalCreate } from './contents/educational/educational-create/educational-create';
+import { EducationalEdit } from './contents/educational/educational-edit/educational-edit';
 
 // Podcasts
-import { PodcastsListComponent } from './podcasts/podcasts-list/podcasts-list';
-import { PodcastValidateComponent } from './podcasts/podcast-validate/podcast-validate';
+import { PodcastsList } from './podcasts/podcasts-list/podcasts-list';
+import { PodcastValidate } from './podcasts/podcast-validate/podcast-validate';
 
 // Settings
-import { LanguagesComponent } from './settings/general/languages/languages';
-import { TranslationsComponent } from './settings/general/translations/translations';
-import { NotificationsComponent } from './settings/notifications/notifications';
-import { QuotasComponent } from './settings/quotas/quotas';
+import { Languages } from './settings/general/languages/languages';
+import { Translations } from './settings/general/translations/translations';
+import { Notifications } from './settings/notifications/notifications';
+import { Quotas } from './settings/quotas/quotas';
 
 // Users
-import { UsersListComponent } from './users/users-list/users-list';
-import { UserProfileComponent } from './users/user-profile/user-profile';
+import { UsersList } from './users/users-list/users-list';
+import { UserProfile } from './users/user-profile/user-profile';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
   // Pages principales
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'login', component: Login },
 
   // Badges
-  { path: 'badges', component: BadgesListComponent },
-  { path: 'badges/create', component: BadgesCreateComponent },
-  { path: 'badges/edit/:id', component: BadgesEditComponent },
+  { path: 'badges', component: BadgesList },
+  { path: 'badges/create', component: BadgesCreate },
+  { path: 'badges/edit/:id', component: BadgesEdit },
 
   // Challenges
-  { path: 'challenges', component: ChallengesListComponent },
-  { path: 'challenges/create', component: ChallengesCreateComponent },
-  { path: 'challenges/edit/:id', component: ChallengesEditComponent },
-  { path: 'participations', component: ParticipationsComponent },
+  { path: 'challenges', component: ChallengesList },
+  { path: 'challenges/create', component: ChallengesCreate },
+  { path: 'challenges/edit/:id', component: ChallengesEdit },
+  { path: 'participations', component: Participations },
 
   // Educational
-  { path: 'educational', component: EducationalListComponent },
-  { path: 'educational/create', component: EducationalCreateComponent },
-  { path: 'educational/edit/:id', component: EducationalEditComponent },
+  { path: 'educational', component: EducationalList },
+  { path: 'educational/create', component: EducationalCreate },
+  { path: 'educational/edit/:id', component: EducationalEdit },
 
   // Podcasts
-  { path: 'podcasts', component: PodcastsListComponent },
-  { path: 'podcasts/validate/:id', component: PodcastValidateComponent },
+  { path: 'podcasts', component: PodcastsList },
+  { path: 'podcasts/validate/:id', component: PodcastValidate },
 
   // Settings
-  { path: 'settings/languages', component: LanguagesComponent },
-  { path: 'settings/translations', component: TranslationsComponent },
-  { path: 'settings/notifications', component: NotificationsComponent },
-  { path: 'settings/quotas', component: QuotasComponent },
+  { path: 'settings/languages', component: Languages },
+  { path: 'settings/translations', component: Translations },
+  { path: 'settings/notifications', component: Notifications },
+  { path: 'settings/quotas', component: Quotas },
 
   // Users
-  { path: 'users', component: UsersListComponent },
-  { path: 'users/profile/:id', component: UserProfileComponent },
+  { path: 'users', component: UsersList },
+  { path: 'users/profile/:id', component: UserProfile },
 
   // Wildcard route (pour une page 404 ou redirection)
   { path: '**', redirectTo: 'dashboard' }
